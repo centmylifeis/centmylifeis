4d59f77a51c84d6dc024c8b7b0096bed93da06d1
cent <89360612+centmylifeis@users.noreply.github.com>
2021-10-02 15:56:06 +0900

    4주차_1
    거북이 움직이기

---
+++ turtle1.py
@@ -0,0 +1,6 @@
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.bk(100)
+t.rt(90)
---
+++ turtle3.py
@@ -0,0 +1,7 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+
