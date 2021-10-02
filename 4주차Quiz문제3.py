1de83f230a433f2cb0f73b9432b0424443b086d5
cent <89360612+centmylifeis@users.noreply.github.com>
2021-10-02 16:42:56 +0900

    4주차 퀴즈 3개
    1~99까지 홀수만 출력

---
+++ 4주차Quiz문제3.py
@@ -0,0 +1,15 @@
+#1~99까지 홀수만 출력되도록
+
+#-----------------------------------
+# 세번 째 : continue와 break문 사용
+i=0
+while True:
+    i+=1
+    if i==100:
+        break    
+    if i%2==0:
+        continue    
+    print(i)
+    
+    
+        
